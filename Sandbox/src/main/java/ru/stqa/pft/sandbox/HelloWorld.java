a59ab709454d4362600e2ee80dabab83a2573bd4
Evgeniya <ZhmenkaX@mail.ru>
2019-09-21 18:20:03 +0300

    Добавлено несколько функций

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,7 +3,27 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Zhenya");
+
+    double len = 5;
+    System.out.println("Пощадь квадрата со стороной " + len + "=" + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Пощадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
 
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
