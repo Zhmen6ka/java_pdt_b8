94f0bac68f96c1fc0e908d8cbe45285772c75bfb
Evgeniya <ZhmenkaX@mail.ru>
2019-09-21 20:42:20 +0300

    В класс Point добавлен метод вычисления расстояния

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/task2/Distance.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/task2/Distance.java
@@ -2,13 +2,10 @@ package ru.stqa.pft.sandbox.task2;
 
 public class Distance {
   public static void main(String[] args) {
-    Point p1 = new Point(2, 2);
-    Point p2 = new Point(6, 6);
-    System.out.println("Расстояние между двумя точками с координатами "
-            + "(" + p1.x + ";" + p1.y + ")" + " и " + "(" + p2.x + ";" + p2.y + ")" + " = " + distance(p1, p2)) ;
-  }
+    Point p1 = new Point(7, 5);
+    Point p2 = new Point(2, 0);
 
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println("Расстояние между двумя точками с координатами "
+            + "(" + p1.x + ";" + p1.y + ")" + " и " + "(" + p2.x + ";" + p2.y + ")" + " = " + p1.distance(p2)) ;
   }
 }
